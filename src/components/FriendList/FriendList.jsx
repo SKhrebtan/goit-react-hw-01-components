53b43adfb0a31dbd181710447e292b72b31c9555
SKhrebtan <shrebtan@gmail.com>
2023-07-25 17:16:58 +0300

    add transactions w/o styles

---
+++ src/components/FriendList/FriendList.jsx
@@ -0,0 +1,26 @@
+import PropTypes from 'prop-types';
+import { FriendListItem } from './FriendListItem';
+
+export const FriendList = ({friends}) => {
+    return (
+        <ul className="friend-list">
+            {friends.map(({ avatar, name, isOnline, id }) => {
+                return (
+                    <FriendListItem
+                        key={id}
+                                 avatar={avatar}
+                    name={name}
+                    isOnline={isOnline}
+                />)
+            })}
+            </ul>
+    )
+}
+
+FriendList.propTypes = {
+    friends: PropTypes.arrayOf(
+          PropTypes.shape({
+          id: PropTypes.number.isRequired
+        })
+    )
+}
